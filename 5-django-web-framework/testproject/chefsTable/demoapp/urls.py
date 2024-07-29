5eb0328ec2d033610bbbd6669ea8ef7aa12b0a98
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-07-29 17:00:09 +0530

    add views for hello and re

--- 5-django-web-framework/testproject/chefsTable/demoapp/urls.py
+++ 5-django-web-framework/testproject/chefsTable/demoapp/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path 
-from . import views 
+from django.urls import path, re_path
+from demoapp import views 
 
 urlpatterns = [ 
     path('', views.index, name='index'), 
+    path('hello', views.hello, name='hello'),
+    re_path(r'^menu/([0-9]{2})/$', views.re_view, name="re_view")
 ] 
--- 5-django-web-framework/testproject/chefsTable/demoapp/views.py
+++ 5-django-web-framework/testproject/chefsTable/demoapp/views.py
@@ -4,4 +4,9 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("Hello from demo app")
 
-# Create your views here.
+def hello(request):
+    return HttpResponse("<h1>Hello</h1><p>Helllooooo.. la la la</p>")
+
+def re_view(request, id):
+    print(dir(request))
+    return HttpResponse(f"hello {id}")
