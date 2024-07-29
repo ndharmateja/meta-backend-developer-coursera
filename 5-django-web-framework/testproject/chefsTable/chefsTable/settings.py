0f44575d3a128d1502c132fb5c829c07c6500b8b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-07-29 17:58:05 +0530

    set debug false

--- 5-django-web-framework/testproject/chefsTable/chefsTable/settings.py
+++ 5-django-web-framework/testproject/chefsTable/chefsTable/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-5-q+9=9nklm3(upycdqd+sz85hmiudp@r05+#4ui^z#fo&f9s0"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
