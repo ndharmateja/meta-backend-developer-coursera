ae5984674179ba4303305a2a0fead33f0d2431bf
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2024-08-20 16:49:31 +0530

    add installed apps

--- 6-apis/week4/littlelemon/littlelemon/settings.py
+++ 6-apis/week4/littlelemon/littlelemon/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "littlelemon_api",
+    "rest_framework",
+    "djoser",
+    "rest_framework.authtoken",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
